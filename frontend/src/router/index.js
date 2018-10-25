5a11eba59e6c797df904341d6de0922a7081afc1
zuby <dvzuby@gmail.com>
2018-10-25 18:58:33 +0900

    폴더링

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from '@/components/Login'
-import Signup from '@/components/Signup'
+import Login from '@/view/home/Login'
+import Signup from '@/view/home/Signup'
 
 Vue.use(Router)
 
